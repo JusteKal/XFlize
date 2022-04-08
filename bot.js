d1508b7270ac20212263e6ae0541950f8462ebfb
Your Name <canasperez.nathanael@gmail.com>
2022-04-08 19:27:18 +0200

    add random status

--- bot.js
+++ bot.js
@@ -12,8 +12,6 @@ for (const file of commandFiles) {
 }
 
 bot.on('ready', () => {
-        bot.user.setActivity("enquêter");
-        bot.user.setStatus('dnd');
         console.log(`Logged in as ${bot.user.tag}!`);
     });
 
@@ -35,6 +33,29 @@ bot.on('guildMemberAdd', guildMember => {
 
 bot.on('guildMemberRemove', guildMember => {
 	guildMember.guild.channels.cache.get('952926518546419723').send(`:red_circle: Bonne continuation, **<@${guildMember.user.id}>!**`);
+});
+
+// changement du jeu du bot de façon aléatoire dans une liste de jeux toutes les heures
+bot.on('ready', () => {
+	setInterval(function() {
+		let random = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+		if (random == 1) {
+			bot.user.setActivity("enquêter");
+			bot.user.setStatus('dnd');
+		} else if (random == 2) {
+			bot.user.setActivity("Sécuriser les lignes");
+			bot.user.setStatus('dnd');
+		} else if (random == 3) {
+			bot.user.setActivity("Écouter vos potins");
+			bot.user.setStatus('dnd');
+		} else if (random == 4) {
+			bot.user.setActivity("Recruter des agents");
+			bot.user.setStatus('dnd');
+		} else if (random == 5) {
+			bot.user.setActivity("Boucler des dossiers");
+			bot.user.setStatus('dnd');
+		}
+	}, 3600000);
 });
 
 bot.login(process.env.TOKEN);
